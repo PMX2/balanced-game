435742439a4c714a06555c428a3cfc030beca681
Noam Zeltser <zeltsernoam@gmail.com>
2019-02-28 15:13:20 +0100

    Premier commit Noam
    Camera FPS + Mouvements  du joueur + Jetpack

---
+++ Balance Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    [SerializeField]
+    private float _lookSensitivityX = 2f;
+    [SerializeField]
+    private float _lookSensitivityY = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    [Header("Joint Options")]
+    [SerializeField]
+    private JointDriveMode jointMode = JointDriveMode.Position;
+
+    [SerializeField]
+    private float jointSpring = 12f;
+    [SerializeField]
+    private float jointMaxForce = 20f;
+
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+        SetJointSettings(jointSpring);
+    }
+
+    private void Update()
+    {
+        // Calcul de la vélocité du joueur en un vecteur 3D
+        float MovX = Input.GetAxisRaw("Horizontal");
+        float MovZ = Input.GetAxisRaw("Vertical");
+        Vector3 HorizontalMove = transform.right * MovX;
+        Vector3 VerticalMove = transform.forward * MovZ;
+        Vector3 Velocity = (HorizontalMove + VerticalMove).normalized * _speed;
+        motor.Move(Velocity);
+        
+        // Calcul de la rotation de la caméra en un vecteur 3D
+        float RotY = Input.GetAxisRaw("Mouse X");
+        Vector3 Rotation = new Vector3(0, RotY, 0) * _lookSensitivityX;
+        motor.Rotate(Rotation);
+        
+        float RotX = Input.GetAxisRaw("Mouse Y");
+        float CameraRotationX = RotX * _lookSensitivityY;
+        motor.RotateCamera(CameraRotationX);
+
+        Vector3 _thrusterForce = Vector3.zero;
+        
+        //Calcul de la force du jetpack
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+            SetJointSettings(0f);
+        }
+        else
+        {
+            SetJointSettings(jointSpring);
+        }
+        //Appliquer la variable trustherforce
+        motor.ApplyThruster(_thrusterForce);
+    }
+
+    private void SetJointSettings(float _jointSpring)
+    {
+        joint.yDrive = new JointDrive {mode = jointMode, positionSpring = _jointSpring, maximumForce = jointMaxForce};
+    }
+}
---
+++ Balance Game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,74 @@
+﻿using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private float camerarotationX = 0f;
+    private float currentCameraRotationX = 0f;
+    [SerializeField]
+    private float cameraRotationLimit = 85f;
+    private Rigidbody rb;
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 thrusterForce;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 Velocity)
+    {
+        velocity = Velocity;
+    }
+    
+    public void Rotate(Vector3 Rotation)
+    {
+        rotation = Rotation;
+    }
+    
+    public void RotateCamera(float CameraRotationX)
+    {
+        camerarotationX = CameraRotationX;
+    }
+
+    public void ApplyThruster(Vector3 _thrusterForce)
+    {
+        thrusterForce = _thrusterForce;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if (thrusterForce != Vector3.zero)
+        {
+            rb.AddForce(thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        //Récupération de la rotation + on empeche la camera de touner en 360
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        currentCameraRotationX -= camerarotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+        cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+}
+ 
